60526b2fc0ac646179676ed4ab40482bb658023a
vatinMC <mc@vatin.de>
2025-01-03 17:38:29 +0100

    added background, used translationkeys for buttons

--- src/main/java/de/vatinmc/cmdconfirm/screen/ConfirmationScreen.java
+++ src/main/java/de/vatinmc/cmdconfirm/screen/ConfirmationScreen.java
@@ -5,6 +5,7 @@ import net.minecraft.client.gui.screen.ChatScreen;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 
 public class ConfirmationScreen extends Screen {
     private int centerX = 0;
@@ -13,33 +14,36 @@ public class ConfirmationScreen extends Screen {
     public ButtonWidget abort;
     private final Screen parent;
     private final String cmd;
+    private final Text confirmText;
 
     public ConfirmationScreen(Screen parent, String cmd) {
         super(Text.literal("Confirmation Screen"));
         this.parent = parent;
         this.cmd = cmd;
+        confirmText = Text.literal("Want to use \"" + cmd + "\"?");
     }
 
     @Override
     protected void init(){
         centerX = width / 2;
         centerY = height / 2;
-        int buttonWidth = 80;
+        int buttonWidth = 50;
         int buttonHeight = 20;
         int marginX = 5;
-        int marginY = 20;
         int buttonX = centerX - buttonWidth - marginX;
-        int buttonY = centerY + buttonHeight + marginY;
+        int buttonY = centerY - buttonHeight / 2;
+        Text buttonText = Text.translatable("gui.yes");
 
-        confirm = ButtonWidget.builder(Text.literal("Confirm"),
+        confirm = ButtonWidget.builder(buttonText,
                 button -> {
                     ChatScreen chatScreen = (ChatScreen) parent;
                     chatScreen.sendMessage("/" + cmd, true);
-                    client.setScreen((Screen) null);
+                    client.setScreen(null);
         }).dimensions(buttonX,buttonY,buttonWidth,buttonHeight).build();
 
         buttonX = centerX + marginX;
-        abort = ButtonWidget.builder(Text.literal("Abort"),
+        buttonText = Text.translatable("gui.no");
+        abort = ButtonWidget.builder(buttonText,
                 button -> {
             close();
         }).dimensions(buttonX,buttonY,buttonWidth,buttonHeight).build();
@@ -51,12 +55,24 @@ public class ConfirmationScreen extends Screen {
     @Override
     public void render(DrawContext drawContext, int mouseX, int mouseY, float delta){
         super.render(drawContext,mouseX,mouseY,delta);
-        drawContext.drawCenteredTextWithShadow(textRenderer, Text.literal("Want to use \"" + cmd + "\"?"), centerX,centerY - 30, 0xffffff);
+        drawContext.drawCenteredTextWithShadow(textRenderer, confirmText, centerX,centerY - 30, 0xffffff);
     }
 
     @Override
     public void renderBackground(DrawContext context, int mouseX, int mouseY, float delta) {
-        this.renderDarkening(context);
+        super.renderBackground(context, mouseX, mouseY, delta);
+        int textWidth = textRenderer.getWidth(confirmText);
+        int x1 = centerX - 5 - textWidth / 2;
+        int y1 = centerY - 30 - 5;
+        int x2 = centerX + 5 + textWidth / 2;
+        int y2 = centerY - 22 + 5;
+        int border = 1;
+
+        context.fill(x1, y1, x2,y2,0xff000000);//black
+        context.fill(x1 + border, y1 + border, x2 - border,y2 - border,0xffffffff);//white
+
+        Identifier texture = Identifier.of("minecraft", "textures/block/obsidian.png");
+        context.drawTexture(texture, x1 + border, y1 + border, 16,16, x2-x1-border*2, y2-y1-border*2, 8,8);
     }
 
     @Override
